fe048fe4286b7662f54020e4ac5a53b9ee182b93
Jan Van Braeckel <janvanbraeckel1995@hotmail.com>
2017-05-14 15:09:46 +0200

    Revert "Use node 6 :( bye async/await"
    This reverts commit a6f4d89793da9ba8acb8bd8e40cb0b873b4fea62.

--- controllers/devCafeController.js
+++ controllers/devCafeController.js
@@ -3,142 +3,132 @@ const request = require('request');
 const Topic = mongoose.model('Topic');
 const Vote = mongoose.model('Vote');
 
-exports.createTopic = (req, res) => {
-  const topic = (new Topic({ name: req.body.text })).save().then((topic) => res.status(200).json({
+exports.createTopic = async (req, res) => {
+  const topic = await (new Topic({ name: req.body.text })).save();
+  res.status(200).json({
     text: `Created topic ${topic.name}`
-  }));
+  });
 };
 
-exports.close = (req, res) => {
-  Vote.findOne({ finished: false }).then((vote) => {
-    if (vote) {
-      const topics = vote.topics.sort((a, b) => b.votedby.length - a.votedby.length);
-      const chosen = topics[0];
-      Topic.findById(chosen._id).then((topic) => {
-        topic.chosen = true;
-        vote.finished = true;
-        let topicPromise = Topic.findByIdAndUpdate(topic._id, topic);
-        let votePromise = Vote.findByIdAndUpdate(vote._id, vote);
-        Promise.all([topicPromise, votePromise]).then(() => res.status(200).json({
-          response_type: 'in_channel',
-          text: `<!channel> Voting has closed! Topic ${topic.name} has won with ${chosen.votedby.length} votes. Thank you for voting for this topic ${chosen.votedby.join(', ')}!`,
-        }));
+exports.close = async (req, res) => {
+  const vote = await Vote.findOne({ finished: false });
+  if (vote) {
+    const topics = vote.topics.sort((a, b) => b.votedby.length - a.votedby.length);
+    const chosen = topics[0];
+    const topic = await Topic.findById(chosen._id);
+    topic.chosen = true;
+    vote.finished = true;
+    await Topic.findByIdAndUpdate(topic._id, topic);
+    await Vote.findByIdAndUpdate(vote._id, vote);
+    res.status(200).json({
+      response_type: 'in_channel',
+      text: `<!channel> Voting has closed! Topic ${topic.name} has won with ${chosen.votedby.length} votes. Thank you for voting for this topic ${chosen.votedby.join(', ')}!`,
+    });
+  } else {
+    res.status(200).json({
+      response_type: 'in_channel',
+      text: 'No voting is currently open, start a new vote with /cafevote!',
+    });
+  }
+};
+
+exports.startVote = async (req, res) => {
+  const existingVote = await tryGetExistingVote();
+  if (existingVote && existingVote.length > 0) {
+    await resendVote(req, res, existingVote[0]);
+  } else {
+    const topics = await findRandomTopics(4);
+    if (topics && topics.length > 0) {
+      const vote = await (new Vote({ topics: topics.map(topic => { return { _id: topic._id } }) })).save();
+      let text = '';
+      const mapped = topics.map((topic, index) => {
+        text += `${index + 1}. ${topic.name} (0 vote(s))\n`;
+        return {
+          name: 'topic',
+          type: 'button',
+          value: topic._id,
+          text: index + 1
+        };
+      });
+      res.status(200).json({
+        response_type: 'in_channel',
+        text: 'Let\'s vote for a topic!',
+        attachments: [{
+          text,
+          callback_id: vote._id,
+          actions: mapped
+        }]
       });
     } else {
       res.status(200).json({
         response_type: 'in_channel',
-        text: 'No voting is currently open, start a new vote with /cafevote!',
+        text: 'There are currently no topics available, add some more!'
       });
     }
-  });
+  }
 };
 
-exports.startVote = (req, res) => {
-  tryGetExistingVote().then((existingVote) => {
-    if (existingVote && existingVote.length > 0) {
-      resendVote(req, res, existingVote[0]);
-    } else {
-      findRandomTopics(4).then((topics) => {
-        if (topics && topics.length > 0) {
-          (new Vote({ topics: topics.map(topic => { return { _id: topic._id } }) })).save().then((vote) => {
-            let text = '';
-            const mapped = topics.map((topic, index) => {
-              text += `${index + 1}. ${topic.name} (0 vote(s))\n`;
-              return {
-                name: 'topic',
-                type: 'button',
-                value: topic._id,
-                text: index + 1
-              };
-            });
-            res.status(200).json({
-              response_type: 'in_channel',
-              text: '<!channel> Let\'s vote for a topic!',
-              attachments: [{
-                text,
-                callback_id: vote._id,
-                actions: mapped
-              }]
-            });
-          });
-        } else {
-          res.status(200).json({
-            response_type: 'in_channel',
-            text: 'There are currently no topics available, add some more!'
-          });
-        }
-      });
-    }
-  });
-};
-
-exports.vote = (req, res) => {
+exports.vote = async (req, res) => {
   const payload = JSON.parse(req.body.payload);
-  Vote.findById(payload.callback_id).then((vote) => {
-    vote.topics.forEach(el => {
-      if (el._id.toString() === payload.actions[0].value) {
-        let votedby = el.votedby;
-        votedby.push(payload.user.name);
-        el.votedby = votedby.filter((el, pos, arr) => arr.indexOf(el) == pos);
-      }
-    });
-    Vote.findByIdAndUpdate(payload.callback_id, vote).then(()=>{
-      let message = payload.original_message;
-      let map = new Map();
-      Promise.all(vote.topics.map((topic, index) => {
-        return Topic.findById(topic._id).then((dbTopic) => {
-          map.set(index, `${index + 1}. ${dbTopic.name} (${topic.votedby.length} vote(s))`);
-          return {
-            name: 'topic',
-            type: 'button',
-            value: topic._id,
-            text: index + 1
-          };
-        });
-      })).then((actions)=>{
-        message.attachments[0].actions = actions;
-        let text = '';
-        new Map([...map.entries()].sort()).forEach((value) => text += `${value}\n`);
-        message.attachments[0].text = text;
-        message.attachments[0].mrkdwn = true;
-        request({
-          method: 'POST',
-          url: payload.response_url,
-          json: true,
-          body: message
-        }, (error, resp, body) => {
-          res.status(200).send();
-        });
-      });
-    });
+  const vote = await Vote.findById(payload.callback_id);
+  vote.topics.forEach(el => {
+    if (el._id.toString() === payload.actions[0].value) {
+      let votedby = el.votedby;
+      votedby.push(payload.user.name);
+      el.votedby = votedby.filter((el, pos, arr) => arr.indexOf(el) == pos);
+    }
+  });
+  await Vote.findByIdAndUpdate(payload.callback_id, vote);
+  let message = payload.original_message;
+  let map = new Map();
+  message.attachments[0].actions = await Promise.all(vote.topics.map(async (topic, index) => {
+    const dbTopic = await Topic.findById(topic._id);
+    map.set(index, `${index + 1}. ${dbTopic.name} (${topic.votedby.length} vote(s))`);
+    return {
+      name: 'topic',
+      type: 'button',
+      value: topic._id,
+      text: index + 1
+    };
+  }));
+  let text = '';
+  new Map([...map.entries()].sort()).forEach((value) => text += `${value}\n`);
+  message.attachments[0].text = text;
+  message.attachments[0].mrkdwn = true;
+  request({
+    method: 'POST',
+    url: payload.response_url,
+    json: true,
+    body: message
+  }, (error, resp, body) => {
+    res.status(200).send();
   });
 };
 
-const resendVote = (req, res, existingVote) => {
-  Topic.find({
+const resendVote = async (req, res, existingVote) => {
+  const topics = await Topic.find({
     '_id': {
       $in: existingVote.topics
     }
-  }).then((topics) => {
-    let text = '';
-    const mapped = existingVote.topics.map((topic, index) => {
-      text += `${index + 1}. ${topics.find(top => top._id.toString() === topic._id.toString()).name} (${topic.votedby.length} vote(s))\n`;
-      return {
-        name: 'topic',
-        type: 'button',
-        value: topic._id,
-        text: index + 1
-      };
-    });
-    res.status(200).json({
-      response_type: 'in_channel',
-      text: 'There\'s already a voting going on! Continue voting here or finish the vote!',
-      attachments: [{
-        text,
-        callback_id: existingVote._id,
-        actions: mapped
-      }]
-    });
+  });
+  let text = '';
+  const mapped = existingVote.topics.map((topic, index) => {
+    text += `${index + 1}. ${topics.find(top => top._id.toString() === topic._id.toString()).name} (${topic.votedby.length} vote(s))\n`;
+    return {
+      name: 'topic',
+      type: 'button',
+      value: topic._id,
+      text: index + 1
+    };
+  });
+  res.status(200).json({
+    response_type: 'in_channel',
+    text: 'There\'s already a voting going on! Continue voting here or finish the vote!',
+    attachments: [{
+      text,
+      callback_id: existingVote._id,
+      actions: mapped
+    }]
   });
 };
 
@@ -154,6 +144,6 @@ const findRandomTopics = (amount = 4) => {
   });
 };
 
-const tryGetExistingVote = () => {
-  return Vote.find({ finished: false });
+const tryGetExistingVote = async () => {
+  return await Vote.find({ finished: false });
 };
